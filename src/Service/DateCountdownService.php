8c690e151d22c95b27212e3a6d426d353be2135e
Seekon <mzajc5@gmail.com>
2020-02-08 11:33:24 +0100

    Add initial service

---
+++ src/Service/DateCountdownService.php
@@ -0,0 +1,16 @@
+<?php
+namespace Drupal\date_countdown\Service;
+
+/**
+ * Simple example of a Service
+ */
+class DateCountdownService {
+
+  /**
+   * return day difference
+   */
+  public function countDayDifference() {
+    return '';
+  }
+
+}
