8afee40096b7a9ea3076c25b66d313fb00b6d483
Seekon <mzajc5@gmail.com>
2020-02-08 15:01:21 +0100

    Implement countdown service calculation function

--- src/Service/DateCountdownService.php
+++ src/Service/DateCountdownService.php
@@ -9,8 +9,15 @@ class DateCountdownService {
   /**
    * return day difference
    */
-  public function countDayDifference() {
-    return '';
+  public function countDayDifference($date) {
+    $datetotime = strtotime($date);
+    if($datetotime == false)
+      return false;
+
+    $remaining = $datetotime - time();
+    $days_remaining = floor($remaining / 86400);
+
+    return $days_remaining;
   }
 
 }
